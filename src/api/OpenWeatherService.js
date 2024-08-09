68d084cd68040141e7e7490d0d61a99ef5c4dd4a
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-08-09 19:50:24 +0530

    installed yarn and updated api keys

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = 'd0d202dfa0feb471d670f7f923520891';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
